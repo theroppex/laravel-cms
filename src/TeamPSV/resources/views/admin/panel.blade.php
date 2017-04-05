0bb95dfca129c95d1fae188a1cbee775d68ebcf8
Petar Rankovic <theroppex@gmail.com>
2017-04-05 10:53:52 +0200

    Modifikovan admin panel

--- src/TeamPSV/resources/views/admin/panel.blade.php
+++ src/TeamPSV/resources/views/admin/panel.blade.php
@@ -5,40 +5,55 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-               <table class="table">
-                   <thead>
-                       <tr>
-                           <th>Id</th>
-                           <th>Name</th>
-                           <th>Surname</th>
-                           <th>Email</th>
-                           <th>Delete</th>
-                       </tr>
-                   </thead>
-                   <tbody>
-                   @foreach($users as $user)
-                       <tr>
-                           <td>
-                               {{$user->id}}
-                           </td>
-                           <td>
-                               {{$user->name}}
-                           </td>
-                           <td>
-                               {{$user->surname}}
-                           </td>
-                           <td>
-                               {{$user->email}}
-                           </td>
-                           <td style="text-align: center">
-                               @if(!\App\User::find($user->id)->isAdmin())
-                                   <a href="/user/delete/{{$user->id}}" ><i class="fa fa-trash" style="color: red" aria-hidden="true"></i></a>
-                               @endif
-                           </td>
-                       </tr>
-                   @endforeach
-                   </tbody>
-               </table>
+                <div class="panel-group" id="accordion">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h4 class="panel-title">
+                                <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+                                    Users
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapse1" class="panel-collapse collapse in">
+                            <div class="panel-body">
+                                <table class="table">
+                                    <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Name</th>
+                                        <th>Surname</th>
+                                        <th>Email</th>
+                                        <th>Delete</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($users as $user)
+                                        <tr>
+                                            <td>
+                                                {{$user->id}}
+                                            </td>
+                                            <td>
+                                                {{$user->name}}
+                                            </td>
+                                            <td>
+                                                {{$user->surname}}
+                                            </td>
+                                            <td>
+                                                {{$user->email}}
+                                            </td>
+                                            <td style="text-align: center">
+                                                @if(!\App\User::find($user->id)->isAdmin())
+                                                    <a href="/user/delete/{{$user->id}}" ><i class="fa fa-trash" style="color: red" aria-hidden="true"></i></a>
+                                                @endif
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
