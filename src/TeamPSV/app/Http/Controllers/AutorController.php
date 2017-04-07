58f95145471231b7786c47eba2545bb57deeec48
Petar Rankovic <theroppex@gmail.com>
2017-04-07 12:34:31 +0200

    Promenjen autor controller

--- src/TeamPSV/app/Http/Controllers/AutorController.php
+++ src/TeamPSV/app/Http/Controllers/AutorController.php
@@ -7,10 +7,15 @@ use App\Http\Controllers\Controller;
 
 class AutorController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('banned');
+    }
+
     public function getTextForm()
     {
         return view("text.form");
-
     }
 
 }
