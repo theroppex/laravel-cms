958811c919fe7ec8861951158c746f3cf637f1c9
Petar Rankovic <theroppex@gmail.com>
2017-04-10 22:14:37 +0200

    Dodati komentari u text tut kontroler

--- src/TeamPSV/app/Http/Controllers/TextTutorialsController.php
+++ src/TeamPSV/app/Http/Controllers/TextTutorialsController.php
@@ -25,21 +25,43 @@ class TextTutorialsController extends Controller
         return view('text.list', ['posts' => $posts]);
     }
 
+    /**
+     * Dohvata postove na osnovu kategorije
+     *
+     * @param Category $category
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function getPostsByCategory(Category $category){
         $posts = TextPost::where('cat-id', $category->id)->paginate(10);
         return view('text.list', ['posts' => $posts]);
     }
 
+    /**
+     * Dohvata sve besplatne postove
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function getAllFreePosts(){
         $posts = TextPost::where('type', 'free')->paginate(10);
         return view('text.list', ['posts' => $posts]);
     }
 
+    /**
+     * Dohvata sve placene postove
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function getAllPaidPosts(){
         $posts = TextPost::where('type', 'paid')->paginate(10);
         return view('text.list', ['posts' => $posts]);
     }
 
+    /**
+     * Korisniku prikazuje besplatan post
+     *
+     * @param TextPost $post
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function getFreePost(TextPost $post){
         return view('text.post', ['post' => $post]);
     }
