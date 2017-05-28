7dc42a5fd90e9de3b2567220736e487b57d6b3be
Stevan <vojvoda_92@hotmail.com>
2017-05-28 14:01:23 +0200

    optimizovana Home stranica

--- src/TeamPSV/database/seeds/CategoryTableSeeder.php
+++ src/TeamPSV/database/seeds/CategoryTableSeeder.php
@@ -12,51 +12,61 @@ class CategoryTableSeeder extends Seeder
      */
     public function run()
     {
+        $path = public_path();
+
         Category::create(
             [
                 'name' => 'cpp',
+                'url' => '/img/c.png',
             ]
         );
 
         Category::create(
             [
                 'name' => 'java',
+                'url' => '/img/java.jpg',
             ]
         );
 
         Category::create(
             [
                 'name' => 'php',
+                'url' => '/img/php.png',
             ]
         );
 
         Category::create(
             [
                 'name' => 'xml',
+                'url' => '/img/xml.png',
             ]
         );
 
         Category::create(
             [
                 'name' => 'js',
+                'url' => '/img/javascript.png',
             ]
         );
 
         Category::create(
             [
                 'name' => 'nodeJS',
+                'url' => '/img/nodejs.png',
             ]
         );
 
         Category::create(
             [
                 'name' => 'html',
+                'url' => '/img/html.png',
             ]
         );
 
         Category::create(
             [
                 'name' => 'jQuery',
+                'url' => '/img/jquery.png',
             ]
         );
     }
